03d9bcaae572e1051677926053c54c71c1b214c0
jotenakis <jba340@proton.me>
2026-05-13 22:10:50 +0200

    36

--- post-install-common.sh
+++ post-install-common.sh
@@ -1927,7 +1927,7 @@ _ENSURE_LVM_SWAP() {
     fi
 
     local swap_dev_real
-    swap_dev_real=$(readlink -f "${swap_dev}" 2>/dev/null) || true
+    swap_dev_real=$(sudo readlink -f "${swap_dev}" 2>/dev/null) || true
     if ! grep -Fxq "${swap_dev_real}" </proc/swaps; then
         swapon "${swap_dev}"
     fi
